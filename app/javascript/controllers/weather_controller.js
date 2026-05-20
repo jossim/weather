abb3df88d978daf93026e059da136c1f0fcabe31
Joseph Simmons <josephdsimmons@gmail.com>
2026-05-20 18:42:28 -0500

    Fixes spacing

--- app/javascript/controllers/weather_controller.js
+++ app/javascript/controllers/weather_controller.js
@@ -3,17 +3,17 @@ import { Controller } from "@hotwired/stimulus"
 export default class WeatherController extends Controller {
 	static targets = [ "forecast" ];
 
-  connect() {
-    console.log("WeatherController connected")
-  }
+	connect() {
+		console.log("WeatherController connected");
+	}
 
-  async submit(event) {
+	async submit(event) {
 		const formData = new FormData(event.target);
 		const params = new URLSearchParams(formData);
 		const url = `/api/weather?${params.toString()}`;
 
-    event.preventDefault()
-    console.log("WeatherController submit")
+		event.preventDefault()
+		console.log("WeatherController submit")
 
 		try {
 			const response = await fetch(url, {
